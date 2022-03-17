994031654ceaf828dc7c9671dcca8c8ac13709f3
Hakan <hknakks1@gmail.com>
2022-03-17 13:52:25 +0300

    navbar menu, sidebar menu, home page, admin dashboard, chart, user information completed

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 1000,
+  },
+  {
+    name: "Feb",
+    "Active User": 4000,
+  },
+  {
+    name: "Mar",
+    "Active User": 1000,
+  },
+  {
+    name: "Apr",
+    "Active User": 4000,
+  },
+  {
+    name: "May",
+    "Active User": 2000,
+  },
+  {
+    name: "Jun",
+    "Active User": 4000,
+  },
+  {
+    name: "Jul",
+    "Active User": 6000,
+  },
+  {
+    name: "Agu",
+    "Active User": 4000,
+  },
+  {
+    name: "Sep",
+    "Active User": 5000,
+  },
+  {
+    name: "Oct",
+    "Active User": 4000,
+  },
+  {
+    name: "Nov",
+    "Active User": 3000,
+  },
+  {
+    name: "Dec",
+    "Active User": 4000,
+  },
+];
